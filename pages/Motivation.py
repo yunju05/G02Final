bf2ff5595501cf3e6b399d1abc5ccecc69057bef
yunju05 <yunju.05.3.25@gmail.com>
2025-05-19 22:02:04 +0900

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,3 +1,4 @@
+import streamlit as st
 url="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20text%20picture.png"
 st.image(url)
 url1="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20%EC%9B%8C%EB%93%9C%20%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C.png"
