bf6a42fc867d610a93b9a73836f55f6d3b5346a8
Jaejoon Lee <dlwownstablet@gmail.com>
2025-05-19 13:40:14 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,3 +1,15 @@
+import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
+
+# GitHub URL에서 이미지 불러오기
+image_url = 'https://github.com/user-attachments/assets/3a4f3c17-aa5b-427a-a505-e6a1abfa493d'
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+# 이미지 표시
+st.image(image, caption='Your Image Caption', use_column_width=True)
+
 print "Welcome to our High-Technology English class!"
 
-![image](https://github.com/user-attachments/assets/3a4f3c17-aa5b-427a-a505-e6a1abfa493d)
