2b68de766ab3a61bf0b2ce9cee957133d94fbeed
Jaejoon Lee <dlwownstablet@gmail.com>
2025-05-19 20:04:16 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,8 +1,8 @@
 import requests
 from io import BytesIO
 
+print("Welcome to our High-Technology English class!")
 st.markdown("# Welcome to our High-Technology English class!")
-st.markdown("# Welcome to Our Digital English Class!")
 
 # GitHub에 업로드된 이미지의 URL
 image_url = 'https://github.com/user-attachments/assets/3a4f3c17-aa5b-427a-a505-e6a1abfa493d'
