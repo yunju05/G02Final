5f8acf46ccf632a6b62cfec89f2b9136319205e9
yunju05 <yunju.05.3.25@gmail.com>
2025-05-19 22:12:12 +0900

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -4,12 +4,16 @@ st.image(url)
 url1="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20%EC%9B%8C%EB%93%9C%20%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C.png"
 st.image(url1)
 
-# 초기화: 세션 상태에 'comments'가 없으면 빈 리스트로 초기화
+import streamlit as st
+
+# 초기화: 세션 상태에 'comments'와 'comment_input'이 없으면 초기화
 if 'comments' not in st.session_state:
     st.session_state['comments'] = []
+if 'comment_input' not in st.session_state:
+    st.session_state['comment_input'] = ''
 
 # 댓글 입력을 위한 텍스트 입력 필드
-comment_input = st.text_input("댓글을 입력하세요:")
+comment_input = st.text_input("댓글을 입력하세요:", key='comment_input')
 
 # '댓글 남기기' 버튼
 if st.button("댓글 남기기"):
@@ -18,9 +22,6 @@ if st.button("댓글 남기기"):
         st.session_state['comments'].append(comment_input)
         # 입력 필드를 초기화하기 위해 세션 상태를 업데이트
         st.session_state['comment_input'] = ''
-        
-# 댓글 입력 필드 업데이트
-st.text_input("댓글을 입력하세요:", key='comment_input')
 
 # 저장된 댓글 표시
 st.write("### 댓글 목록")
