f381a092500d7aa91b457583aa0c700a2be060f5
yeeunk <mirr050423@gmail.com>
2025-05-19 19:28:10 +0900

    Update Words and Expressions.py

--- pages/Words and Expressions.py
+++ pages/Words and Expressions.py
@@ -6,7 +6,7 @@ import random
 
 st.write("🌱 Vocabulary learning")
 
-tab1, tab2, tab3, tab4 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice", "❄️ 4. TBA"])
+tab1, tab2, tab3, tab4 = st.tabs(["1. Lesson: Word list", "2. Activity: Listen to the word", "3. Spelling practice", "4. TBA"])
 
 ######### TAB 1
 
