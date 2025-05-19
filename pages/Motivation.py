65fe41e5fe61dbd854c2465e6bd772b7d7b3a7a4
yunju05 <yunju.05.3.25@gmail.com>
2025-05-19 22:14:35 +0900

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -6,24 +6,23 @@ st.image(url1)
 
 import streamlit as st
 
-# 초기화: 세션 상태에 'comments'와 'comment_input'이 없으면 초기화
+# 초기화: 세션 상태에 'comments'가 없으면 빈 리스트로 초기화
 if 'comments' not in st.session_state:
     st.session_state['comments'] = []
-if 'comment_input' not in st.session_state:
-    st.session_state['comment_input'] = ''
 
 # 댓글 입력을 위한 텍스트 입력 필드
-comment_input = st.text_input("댓글을 입력하세요:", key='comment_input')
+comment_input = st.text_input("댓글을 입력하세요:")
 
 # '댓글 남기기' 버튼
 if st.button("댓글 남기기"):
     # 댓글이 비어있지 않으면 세션 상태에 추가
     if comment_input:
         st.session_state['comments'].append(comment_input)
-        # 입력 필드를 초기화하기 위해 세션 상태를 업데이트
-        st.session_state['comment_input'] = ''
+        # 입력 필드를 초기화
+        st.text_input("댓글을 입력하세요:", value='', key='comment_input')
 
 # 저장된 댓글 표시
 st.write("### 댓글 목록")
 for idx, comment in enumerate(st.session_state['comments']):
     st.write(f"{idx + 1}. {comment}")
+
