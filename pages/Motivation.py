dc3d0a4aeb3e807d3f27f8d926f6e70a875e7456
yunju05 <yunju.05.3.25@gmail.com>
2025-05-20 16:04:57 +0900

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,14 +1,7 @@
 import streamlit as st
-import pandas as pd
 
 st.write("Motivation")
 
-tab1, tab2 = st.tabs([
-    "1. Story Guessing ", 
-    "2. Story Listening "])
-
-######### TAB 1: Word List #########
-with tab1:
     url="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20text%20picture.png"
     st.image(url)
     url1="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20%EC%9B%8C%EB%93%9C%20%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C.png"
@@ -30,8 +23,3 @@ with tab1:
     for idx, comment in enumerate(st.session_state['comments']):
     st.write(f"{idx + 1}. {comment}")
 
-
-            
-######### TAB 1: Word List #########
-with tab2:
-    st.write("Let's listening the story!")
