ffb52784a04cc6a27ff5060cf5294ff82c31d4be
yunju05 <yunju.05.3.25@gmail.com>
2025-05-20 16:08:18 +0900

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -6,7 +6,7 @@ url="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20te
 st.image(url)
 url1="https://github.com/yunju05/G02Final/raw/main/images/%EB%94%94%EB%A6%AC%20%EC%9B%8C%EB%93%9C%20%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C.png"
 st.image(url1)
-st.write("Let's guessing the content of this story and share it!")
+st.write("# Let's guessing the content of this story and share it!")
     
 import streamlit as st
 
