4ff4d2472c42c68292458602551324f6539ba1a9
Jaejoon Lee <dlwownstablet@gmail.com>
2025-05-21 14:51:16 +0900

    Update 2 Story.py

--- pages/2 Story.py
+++ pages/2 Story.py
@@ -1,25 +1,23 @@
 import streamlit as st
 import requests
-from gtts import gTTS
-from io import BytesIO
 
 tab1, tab2 = st.tabs([
     "1. Listening to the story", 
-    "2. reading the story",
+    "2. Reading the story",
 ])
 
 with tab1:
-st.title("### Let's listen to the story!")
+    st.title("### Let's listen to the story!")
 
-def play_audio():
-    st.wirte("Story Audio")
+    def play_audio():
+        st.write("Story Audio")
 
-    # URL에서 파일 다운로드
-    url = "https://github.com/yunju05/G02Final/raw/main/pages/audio_sample.mp3"
-    response = requests.get(url)
+        # URL에서 파일 다운로드
+        url = "https://github.com/yunju05/G02Final/raw/main/pages/audio_sample.mp3"
+        response = requests.get(url)
 
-    # 오디오 재생
-    st.audio(response.content, format='audio/mp3')
+        # 오디오 재생
+        st.audio(response.content, format='audio/mp3')
 
-if __name__ == "__main__":
-    play_audio()
+    if __name__ == "__main__":
+        play_audio()
