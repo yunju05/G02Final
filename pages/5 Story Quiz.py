ce46140d7e5dbdf3470d5e36078b89956a19130e
Jaejoon Lee <dlwownstablet@gmail.com>
2025-06-03 15:48:04 +0900

    Update 5 Story Quiz.py

--- pages/5 Story Quiz.py
+++ pages/5 Story Quiz.py
@@ -11,7 +11,7 @@ sentences = [
     ["As", "they", "walked", "deeper", "into", "the", "woods", ",", "the", "trees", "started", "whispering"],
     ["Each", "tree", "told", "stories", "of", "ancient", "times", ",", "of", "battles", "fought", "and", "lovers", "separated"],
     ["The", "trees", "also", "warned", "them", "about", "the", "dangers", "of", "forgetting", "the", "past", "and", "the", "importance", "of", "nature"],
-    ["Moved", "by", "these", "stories", "," "the", "friends", "promised", "to", "protect", "the", "woods", "and", "share", "their", "knowledge"],
+    ["Moved", "by", "these", "stories", ",", "the", "friends", "promised", "to", "protect", "the", "woods", "and", "share", "their", "knowledge"],
     ["They", "left", "the", "woods", "wiser", "," "with", "a", "deeper", "respect", "for", "nature", "and", "its", "untold", "stories", ",", "ready", "to", "advocate", "for", "its", "preservation"]
 ]
 
