d215ebb0502eaa426213a55d0f7b9f0ffab2cad8
yunju05 <yunju.05.3.25@gmail.com>
2025-06-23 17:44:39 +0900

    Update 2 Selfstudy(pre-class).py

--- pages/2 Selfstudy(pre-class).py
+++ pages/2 Selfstudy(pre-class).py
@@ -4,7 +4,6 @@ import numpy as np
 from gtts import gTTS
 from io import BytesIO
 import random
-import requests
 
 st.write("⭐ Vocabulary learning")
 
@@ -178,11 +177,9 @@ with tab5:
     # 퍼즐 이미지 불러오기 (문제 이미지)
     st.image(
         "https://github.com/yunju05/G02Final/raw/main/images/%ED%81%AC%EB%A1%9C%EC%8A%A4%EC%9B%8C%EB%93%9C.png", 
-        caption="🧩 Crossword Puzzle"
-    
+        caption="🧩 Crossword Puzzle : you can download the image by right-clicking with your mouse or touchpad"
     )
     
-    st.write("you can download the image by right-clicking with your mouse or touchpad")
 
     st.subheader("📝 Definitions")
 
